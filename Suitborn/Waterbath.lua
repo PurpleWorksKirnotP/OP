e9d799db355095c71629f0d6f16421734e7d7f19
PurpleWorks <marcrbxbusiness@gmail.com>
2026-06-20 12:06:55 +0800

    Update Waterbath.lua

--- Suitborn/Waterbath.lua
+++ Suitborn/Waterbath.lua
@@ -72,7 +72,7 @@ NewFeatures:addButton("ESCAPE!!!", function()
 end)
 
 local farmingKatanas = false
-NewFeatures:addButton("Auto-Farm Katanas", function()
+NewFeatures:addButton("Auto-Farm Katanas", function(value)
     farmingKatanas = value
     if farmingKatanas then
         task.spawn(function()
